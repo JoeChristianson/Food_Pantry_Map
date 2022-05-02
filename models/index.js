5636f932465d48b0bef471f735ee76cee5c841a5
Brady Mavetz <b.mavetz@outlook.com>
2022-05-02 11:20:15 -0500

    added pantry and request models, and relationship in index

--- models/index.js
+++ models/index.js
@@ -1,3 +1,23 @@
 const User = require("./User.js");
+const Pantry = require("./Pantry");
+const Request = require("./Request");
 
-module.exports = {User};
+Pantry.belongsTo(User,{
+    foreignKey: "user_id",
+});
+
+User.hasOne(Pantry, {
+    foreignKey: "user_id",
+});
+
+Pantry.hasMany(Request, {
+    foreignKey: "pantry_id",
+});
+
+Request.belongsTo(Pantry, {
+    foreignKey: "pantry_id",
+});
+
+
+
+module.exports = {User, Pantry, Request};
