67e2ead97d2d6c13230e23faf6f3cc11e4228cb3
Crucix <fchang1720@gmail.com>
2022-05-02 13:08:39 -0500

    saved data

---
+++ seeds/pantry_seeds.js
@@ -0,0 +1,21 @@
+const {Pantry} = require('../models');
+
+const pantryData = [
+    {
+        pantry_name: 'UofM',
+        street_address: '123 Univeristy Ave N',
+        city: 'Minneapolis',
+        latitude: 44.971829446,
+        longitude: -93.233832398,
+        contact_phone: '612-123-4567'
+        
+    },
+    {
+        pantry_name: 'HCMC',
+        street_address: '321 Hospital Ave NE',
+        city: 'Minneapolis',
+        latitude: 44.9725278,
+        longitude: -93.2620452,
+        contact_phone: '612-321-7654'
+    }
+]
