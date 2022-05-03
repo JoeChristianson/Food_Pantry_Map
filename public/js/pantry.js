60dda31ffd6a7f72e5d413066ed7db8825e4ce19
Brady Mavetz <b.mavetz@outlook.com>
2022-05-03 10:30:25 -0500

    creating pantry js for pantry handlebar

--- public/js/pantry.js
+++ public/js/pantry.js
@@ -1,6 +1,7 @@
 const pName = document.querySelector("#name")
 const address = document.querySelector("#address")
 const city = document.querySelector("#city")
+const pantryReq = document.querySelector("#pantry_requests")
 
 
 const getPantryData = async ()=>{
@@ -8,12 +9,14 @@ const getPantryData = async ()=>{
     console.log(response)
     const data = await response.json()
     console.log(data)
+    console.log(data.requests[0].product_name);
     populateInfo(data)
 }
 
 getPantryData()
 
 const populateInfo = (data)=>{
+    // var item = 
     pName.innerHTML = data['pantry_name'];
     address.innerHTML = data['street_address'];
     city.innerHTML = data['city']
