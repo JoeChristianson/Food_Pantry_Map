95258ad0cba8c5501493ac3423c97e4bf7ddf1ec
Joe Christianson <jcfargond@gmail.com>
2022-05-04 11:48:36 -0500

    added miles

--- util/distance.js
+++ util/distance.js
@@ -14,7 +14,7 @@ function distanceMath(lat1, lon1, lat2, lon2) {
             c(lat1 * p) * c(lat2 * p) * 
             (1 - c((lon2 - lon1) * p))/2;
   
-    return 12742 * Math.asin(Math.sqrt(a)); // 2 * R; R = 6371 km
+    return (12742 * Math.asin(Math.sqrt(a))/1.609); // 2 * R; R = 6371 km
   }
 
   module.exports = distance;
