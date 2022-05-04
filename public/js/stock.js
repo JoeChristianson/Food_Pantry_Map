21fb75b50bda33e1e3b5ebb37d7a6af27dd51adb
Brady Mavetz <b.mavetz@outlook.com>
2022-05-04 14:13:12 -0500

    edit stock.js to show success display when adding items

--- public/js/stock.js
+++ public/js/stock.js
@@ -1,29 +1,13 @@
 const pantryPage = $(".pantry_page");
-const pName = document.querySelector("#name");
-const address = document.querySelector("#address");
-const city = document.querySelector("#city");
 const closedReqTitle = document.querySelector(".closed_requests")
 const closedReq = document.querySelector("#closed_requests");
-const activateReq = $("#closed_requests")
-const getPantryData = async ()=>{
-    const response = await fetch('api/pantry');
-    console.log(response)
-    const data = await response.json()
-    console.log(data)
-    populateInfo(data)
-}
+const addedMessage = document.querySelector(".isAddedMessage");
+//const isAdded = document.querySelector(".isAdded");
+const activateReq = $("#closed_requests");
 
-getPantryData()
-
-const populateInfo = (data)=>{
-    pName.innerHTML = data['pantry_name'];
-    address.innerHTML = data['street_address'];
-    city.innerHTML = data['city'];
-    
-    getClosedReq();
-}
 
 const getClosedReq = async ()=>{
+    addedMessage.innerHTML = '';
     const response = await fetch('api/request/closed');
     //console.log(response)
     const data = await response.json()
@@ -33,9 +17,9 @@ const getClosedReq = async ()=>{
     }else{
         closedReqTitle.innerHTML = "Closed Request List: ";
         data.forEach(element => {
-        
-        showItem(element);
-    });
+            
+            showItem(element);
+        });
     }
 }
 
@@ -58,36 +42,40 @@ const activateRequest = async (event) =>{
         location.reload();
       } else {
         console.log(response)
-        alert('Failed to sign up.');
+       //add span to show request not activated
       }
 }
 
 const createRequest = async (event) => {
     event.preventDefault();
-
+    
     const productName = document.querySelector('#item_name').value.trim();
     const amount = document.querySelector('#amount').value.trim();
-        console.log(`${productName}, ${amount}`);
+    console.log(`${productName}, ${amount}`);
     if (productName && amount){
         const response = await fetch('/api/request', {
             method: 'POST',
             body: JSON.stringify({ productName, amount }),
             headers: { 'Content-Type': 'application/json' },
-          });
-          if (response.ok) {
+        });
+        if (response.ok) {
             console.log(response)
-            alert('Request Successfully Created');
-          } else {
-            alert('Failed to create request');
-          }
+            addedMessage.innerHTML = `Successfully added the following request: Item - ${productName}, Amount - ${amount}.`;
+            document.querySelector('#item_name').value = '';
+            document.querySelector('#amount').value = '';
+        } else {
+            addedMessage.innerHTML = "Failed to create request. Verify both fields are filled in.";
         }
+    }else{
+        addedMessage.innerHTML = "Failed to create request. Verify both fields are filled in.";
     }
+}
 
 
 pantryPage.on("click", function(){
-    //console.log("next page");
     document.location.replace('/pantry')
 });
 
 activateReq.on("click", ".activate", activateRequest);
 document.querySelector('.request-form').addEventListener('submit', createRequest); 
+getClosedReq();
