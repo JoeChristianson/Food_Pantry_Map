8ba3f140eac9092956ed91f89b5891ec7513fbbc
Brady Mavetz <b.mavetz@outlook.com>
2022-05-04 10:30:51 -0500

    added pantry address to editStock page

--- public/js/stock.js
+++ public/js/stock.js
@@ -1,6 +1,29 @@
 const pantryPage = $(".pantry_page");
+const pName = document.querySelector("#name")
+const address = document.querySelector("#address")
+const city = document.querySelector("#city")
+
+const getPantryData = async ()=>{
+    const response = await fetch('api/pantry');
+    console.log(response)
+    const data = await response.json()
+    console.log(data)
+    populateInfo(data)
+}
+
+getPantryData()
+
+const populateInfo = (data)=>{
+    pName.innerHTML = data['pantry_name'];
+    address.innerHTML = data['street_address'];
+    city.innerHTML = data['city'];
+    // data.requests.forEach(element => {
+        
+    //     addItem(element);
+    // });
+}
 
 pantryPage.on("click", function(){
     console.log("next page");
     document.location.replace('/pantry')
-} );
+});
